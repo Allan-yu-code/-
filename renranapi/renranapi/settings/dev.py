ff5299dcde16268f38d08e2694a4857a107be4e1
moluo <649641514@qq.com>
2020-04-02 07:56:31 +0800

    数据库配置

--- renranapi/renranapi/settings/dev.py
+++ renranapi/renranapi/settings/dev.py
@@ -75,9 +75,17 @@ WSGI_APPLICATION = 'renranapi.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "HOST": "127.0.0.1",
+        "PORT": 3306,
+        "USER": "renran_user",
+        "PASSWORD": "renran",
+        "NAME": "renran",
     }
 }
 
